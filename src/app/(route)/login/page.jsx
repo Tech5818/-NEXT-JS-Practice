5e94d6581ccdc8d97cadade7127c685fc8df8be6
Tech5818 <pqow5818@gmail.com>
2023-12-31 22:22:17 +0900

    :tada: 프로젝트 생성

---
+++ src/app/(route)/login/page.jsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useEffect } from "react"
+
+export default function LoginPage() {
+    
+    useEffect(() => {
+        console.log("mount");
+    }, [])
+    
+    return (
+        <>
+            login
+        </>
+    )
+}
